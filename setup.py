63b744c4725408b3ace1b9fb075aebfbac3d3d7e
cgarciae <cgarcia.e88@gmail.com>
2017-04-27 16:50:41 -0500

    save

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 from setuptools import setup
 from pip.req import parse_requirements
+from tfinterface import __version__
 
 # parse requirements
 reqs = [str(r.req) for r in parse_requirements("requirements.txt", session=False)]
@@ -13,11 +14,11 @@ reqs = [str(r.req) for r in parse_requirements("requirements.txt", session=False
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-version = read('tfinterface/version.txt').split("\n")[0]
+
 
 setup(
     name = "tfinterface",
-    version = version,
+    version = __version__,
     author = "Cristian Garcia",
     author_email = "cgarcia.e88@gmail.com",
     description = ("A light wrapper over TensorFlow that enables you to easily create complex deep neural networks using the Builder Pattern through a functional fluent immutable API"),
