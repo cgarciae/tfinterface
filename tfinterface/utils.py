231f3d287107ef8a72e27c0f42a81bbe2fa7138e
cgarciae <cgarcia.e88@gmail.com>
2017-06-13 21:30:02 -0500

    save

--- tfinterface/utils.py
+++ tfinterface/utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# __coconut_hash__ = 0x83f129d6
+# __coconut_hash__ = 0xe07c8438
 
 # Compiled with Coconut version 1.2.3-post_dev5 [Colonel]
 
@@ -85,7 +85,7 @@ See: https://en.wikipedia.org/wiki/Huber_loss
     return tf.where(tf.abs(x) <= d, 0.5 * tf.square(x), d * (tf.abs(x) - 0.5 * d))
 
 def make_dirs_for_path(path):
-    (os.makedirs())(os.path.dirname(path))
+    (os.makedirs)(os.path.dirname(path))
 
 
 def get_global_step():
