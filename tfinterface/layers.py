3beafab8224b9e343670f20635a166cfc2ee09b5
cgarciae <cgarcia.e88@gmail.com>
2017-06-20 16:28:51 -0500

    batch norm layers

---
+++ tfinterface/layers.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __coconut_hash__ = 0x368af68f
+
+# Compiled with Coconut version 1.2.3-post_dev1 [Colonel]
+
+# Coconut Header: --------------------------------------------------------
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+import sys as _coconut_sys, os.path as _coconut_os_path
+_coconut_file_path = _coconut_os_path.dirname(_coconut_os_path.abspath(__file__))
+_coconut_sys.path.insert(0, _coconut_file_path)
+from __coconut__ import _coconut, _coconut_MatchError, _coconut_tail_call, _coconut_tco, _coconut_igetitem, _coconut_compose, _coconut_pipe, _coconut_starpipe, _coconut_backpipe, _coconut_backstarpipe, _coconut_bool_and, _coconut_bool_or, _coconut_minus, _coconut_map, _coconut_partial
+from __coconut__ import *
+_coconut_sys.path.remove(_coconut_file_path)
+
+# Compiled Coconut: ------------------------------------------------------
+
+import tensorflow as tf
+
+def dense_batch_norm(*args, **kwargs):
+
+    name = kwargs.setdefault("name", "dense_batch_norm")
+    activation = kwargs.pop("activation", None)
+    bn_kwargs = kwargs.pop("bn_kwargs", {})
+
+    with tf.variable_scope(name):
+        net = tf.layers.dense(*args, **kwargs)
+        net = tf.layers.batch_normalization(net, **bn_kwargs)
+
+        return activation(net) if activation else net
+
+def conv2d_batch_norm(*args, **kwargs):
+
+    name = kwargs.setdefault("name", "dense_batch_norm")
+    activation = kwargs.pop("activation", None)
+    bn_kwargs = kwargs.pop("bn_kwargs", {})
+
+    with tf.variable_scope(name):
+        net = tf.layers.conv2d(*args, **kwargs)
+        net = tf.layers.batch_normalization(net, **bn_kwargs)
+
+        return activation(net) if activation else net
