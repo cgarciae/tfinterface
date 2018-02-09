47b20d4d87f3b54bbb28ffde25f35eb03720f2aa
Cristian Garcia <cgarcia.e88@gmail.com>
2018-02-09 11:06:08 -0800

    fix streaming confusion matrix

--- tfinterface/metrics.py
+++ tfinterface/metrics.py
@@ -34,18 +34,12 @@ def streaming_confusion_matrix(labels, predictions, weights=None, num_classes=No
     print(labels, predictions)
     batch_confusion = tf.confusion_matrix(labels, predictions, num_classes=num_classes, name='batch_confusion')
 
-    count = _create_local_variable(None,(),dtype=tf.int32)
-    confusion = _create_local_variable('streamConfusion', [num_classes, num_classes], dtype=tf.int32)
+    confusion = _create_local_variable('stream_confusion', [num_classes, num_classes], dtype=tf.int32)
 
     # Create the update op for doing a "+=" accumulation on the batch
-    count_update = count.assign(count + tf.reduce_sum(batch_confusion))
-    confusion_update = confusion.assign(confusion + batch_confusion)
+    update_op = confusion.assign(confusion + batch_confusion)
 
-    update_op = tf.group(confusion_update, count_update)
-
-    percent_confusion = 100 * tf.truediv(confusion, count)
-
-    return percent_confusion, update_op
+    return confusion, update_op
 
 def r2_score(predictions, labels):
 
