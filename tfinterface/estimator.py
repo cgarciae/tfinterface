6c8cd71c27da76f77a895fce1d492b201222a71e
Cristian Garcia <cgarcia.e88@gmail.com>
2018-03-01 17:45:21 -0800

    added UFFGenerator + UFFPredictor

--- tfinterface/estimator.py
+++ tfinterface/estimator.py
@@ -1,4 +1,13 @@
 import tensorflow as tf
+import uff
+import pycuda.driver as cuda
+import pycuda.autoinit
+import os
+import tensorrt as trt
+
+
+trt.lite.Engine
+
 
 class CheckpointPredictor(object):
     """ Inference class to abstract evaluation """
@@ -28,13 +37,68 @@ class CheckpointPredictor(object):
 
         return self.sess.run(self.predictions, feed_dict = feed_dict)
 
-# class UFFGenerator(object):
-#     def __init__(self, model_dir, uff_path ):
-#         pass
+class UFFGenerator(object):
 
-#     def dump(self):
-#         try:
-#             with open(self.uff_path, x) as f
+    def __init__(self, input_fn, model_fn, model_dir, params, sess = None):
+        self.sess = sess
+        self.input_fn = input_fn
+        self.model_fn = model_fn
+        self.model_dir = model_dir
+        self.params = params
+
+        self.features = self.input_fn()
+
+        if isinstance(self.features, tuple) and len(self.features) == 2:
+            self.features, _ = self.features
+
+        spec = self.model_fn(self.features, None, tf.estimator.ModeKeys.PREDICT, self.params)
+
+        self.predictions = spec.predictions
+
+
+
+    def dump(self, uff_path, model_outputs):
+
+        with tf.Session() as sess:
+            saver = tf.train.Saver()
+            path = tf.train.latest_checkpoint(self.model_dir)
+            saver.restore(sess, path)
+
+            model_graph = sess.graph.as_graph_def()
+
+            model_graph = tf.graph_util.convert_variables_to_constants(sess, model_graph, model_outputs)
+            model_graph = tf.graph_util.remove_training_nodes(model_graph)
+
+            uff_model = uff.from_tensorflow(model_graph, model_outputs)
+
+        assert(uff_model)
+
+        path_parts = uff_path.split(os.sep)
+
+        if len(path_parts) > 1:
+            uff_folder = os.path.join(*path_parts[:-1])
+
+            if not os.path.exists(uff_folder):
+                os.makedirs(uff_folder)
+
+        with open(uff_path, 'w') as f:
+            f.write(uff_model)
+
+    
+
+class UFFPredictor(object):
+    def __init__(self, uff_path, input_nodes, output_nodes, **kwargs):
+        
+        self.engine = trt.lite.Engine(
+            framework = "uff", 
+            path = uff_path, 
+            input_nodes = input_nodes, 
+            output_nodes = output_nodes,
+            **kwargs
+        )
+    
+    def predict(self, *args):
+        return self.engine.infer(*args)
 
 
 # class UFFPredictor(object):
